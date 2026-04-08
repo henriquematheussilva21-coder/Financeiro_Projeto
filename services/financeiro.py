18cdc1017721def43ff2a4ac753d987636eb53b3
henrique.matheus <henriquematheussilva.21@gmail.com>
2026-04-08 17:24:35 -0300

    feat: implementa persistencia de dados com arquivo json

---
+++ services/financeiro.py
@@ -0,0 +1,49 @@
+import json
+import os
+from models.transacao import Transacao
+
+ARQUIVO_DADOS = "data/transacoes.json"
+
+class FinanceiroService:
+    """Centraliza a regra de negócio do sistema."""
+
+
+    def __init__(self):
+        self.transacoes = []
+        self._garantir_pasta_data()
+        self.carregar_dados()
+
+
+    def _garantir_pasta_data(self):
+        """Cria a pasta data se ela ainda não existir"""
+        os.makedirs("data", exist_ok=True)
+
+
+    def salvar_dados(self):
+        """Salva as transações em JSON."""
+        dados = [transacao.to_dict() for transacao in self.transacoes]
+
+        with open(ARQUIVO_DADOS, "w", encoding="utf-8") as arquivo:
+            json.dump(dados, arquivo, ensure_ascii=False, indent=4)
+
+
+    def carregar_dados(self):
+        """Carrega o conteúdo do JSON, caso ele já exista."""
+        if not os.path.exists(ARQUIVO_DADOS):
+            return
+        
+        with open(ARQUIVO_DADOS, "r", encoding="utf-8") as arquivo:
+            try:
+                dados = json.load(arquivo)
+                self.transacoes = [Transacao.from_dict (item) for item in dados]
+            except json.JSONDecodeError:
+                # Preferi não quebrar o programa se o JSON vier vazio ou corrompido.
+                self.transacoes = []
+
+
+    def _proximo_id(self):
+        """Gera o próximo id da lista."""
+        if not self.transacoes:
+            return 1
+        
+        return max(transacao.id for transacao in self.transacoes) + 1
